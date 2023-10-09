1cd46b1ae8d976bd960f2ddec87c8d176fad4870
alu0101311135 <alu0101311135@ull.edu.es>
2023-10-09 10:59:27 +0100

    Cambios menores

--- Sistema_de_Recomendación/metricas.py
+++ Sistema_de_Recomendación/metricas.py
@@ -1,32 +1,20 @@
 import numpy as np
 
 def pearson(r1, r2):
-  # Calcula la correlación de Pearson
-  correlation = np.corrcoef(r1, r2)[0, 1]
-
-  print(f"La correlación de Pearson entre los dos conjuntos de datos es: {correlation}")
+  return np.corrcoef(r1, r2)[0, 1]
 
 def cosinedistance(r1, r2):
-  # Calcular el producto escalar de los vectores
   producto_escalar = np.dot(r1, r2)
 
-  # Calcular las normas de los vectores
   norma_a = np.linalg.norm(r1)
   norma_b = np.linalg.norm(r2)
 
-  # Calcular la distancia del coseno
-  distancia_coseno = producto_escalar / (norma_a * norma_b)
-
-  print("Distancia del coseno:", distancia_coseno)
+  return producto_escalar / (norma_a * norma_b)
 
 def euclideandistance(r1, r2):
-  # Calcular la distancia euclidiana
   distancia = np.linalg.norm(r2 - r1)
 
-  inver_distancia = 1/distancia
-
-  # Imprimir el resultado
-  print(f"La distancia euclidiana entre los vectores {r1} y {r2} es {inver_distancia}")
+  return 1/distancia
 
 if __name__ == '__main__':
   matrix = [[5, 3, 4, 4, "-"],
@@ -35,18 +23,16 @@ if __name__ == '__main__':
             [3, 3, 1, 5, 4],
             [1, 5, 5, 2, 1]]
 
-  # Supongamos que tienes dos conjuntos de datos
-  data1 = np.array(matrix[0][:-1])
-  data2 = np.array(matrix[1][:-1])
+  user1 = np.array(matrix[0][:-1])
+  user2 = np.array(matrix[1][:-1])
+  user3 = np.array(matrix[2][:-1])
+  user4 = np.array(matrix[3][:-1])
 
-  # Calcular la correlación de Pearson
-  pearson(data1, data2)
+  print(f"Correlacion de Pearson entre {user1} y {user2} =", pearson(user1, user2))
 
-  # Calcular la distancia del coseno
-  cosinedistance(data1, data2)
+  print(f"Distancia del coseno entre {user1} y {user2}  =", cosinedistance(user1, user2))
 
-  # Calcular la distancia euclidiana
-  euclideandistance(data1, data2)
+  print(f"Distancia euclidiana entre {user1} y {user2}  =", euclideandistance(user1, user2))
 
 
 
