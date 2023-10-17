2eb15063a3b71b767f7a312c5e16106f2b15a7a4
Pablo400-glitch <alu0101311135@ull.edu.es>
2023-10-17 13:29:45 +0100

    Interfaz Mejorada

--- Sistema_de_Recomendación/menus.py
+++ Sistema_de_Recomendación/menus.py
@@ -1,44 +1,50 @@
-def metrics_menu():
-  metricas_disponibles = ['Correlacion de Pearson', 'Distancia Coseno', 'Distancia Euclidea']
-  print("Elige una métrica:")
-  for i, metrica in enumerate(metricas_disponibles):
-    print(f"{i + 1}. {metrica}")
-    
-  while True:
-    try:
-      seleccion = int(input("Ingrese el número de la métrica deseada: "))
-      if 1 <= seleccion <= len(metricas_disponibles):
-        print(f"\nHas seleccionado: {metricas_disponibles[seleccion - 1]}\n")
-        return metricas_disponibles[seleccion - 1]
-      else:
-        print("Opción no válida. Por favor, seleccione un número válido.")
-    except ValueError:
-      print("Opción no válida. Por favor, seleccione un número válido.")
+from termcolor import colored
 
-def predictions_menu():
-    predicciones_disponibles = ['Predicción simple', 'Diferencia con la media']
-    print("Elige una predicción:")
-    for i, prediccion in enumerate(predicciones_disponibles):
-        print(f"{i + 1}. {prediccion}")
+def metrics_menu():
+    metricas_disponibles = ['Correlacion de Pearson', 'Distancia Coseno', 'Distancia Euclidea']
+    print(colored("Elige una métrica:", 'green'))
+    for i, metrica in enumerate(metricas_disponibles):
+        print(colored(f"{i + 1}. {metrica}", 'cyan'))
     
     while True:
         try:
-            seleccion = int(input("Ingrese el número de la predicción deseada: "))
-            if 1 <= seleccion <= len(predicciones_disponibles):
-                  print(f"\nHas seleccionado: {predicciones_disponibles[seleccion - 1]}\n")
-                  return predicciones_disponibles[seleccion - 1]
+            seleccion = int(input(colored("Ingrese el número de la métrica deseada: ", 'yellow')))
+            if 1 <= seleccion <= len(metricas_disponibles):
+                seleccion_metrica = metricas_disponibles[seleccion - 1]
+                mensaje = "Has seleccionado: "
+                print(colored("\n" + mensaje, 'green') + colored(seleccion_metrica + "\n", 'magenta'))
+                return seleccion_metrica
             else:
-                print("Opción no válida. Por favor, seleccione un número válido.")
+                print(colored("Opción no válida. Por favor, seleccione un número válido.", 'red'))
         except ValueError:
-            print("Opción no válida. Por favor, seleccione un número válido.")
+            print(colored("Opción no válida. Por favor, seleccione un número válido.", 'red'))
+
+def predictions_menu():
+    predicciones_disponibles = ['Predicción simple', 'Diferencia con la media']
+    print(colored("Elige una predicción:", 'green'))
+    for i, prediccion in enumerate(predicciones_disponibles):
+        print(colored(f"{i + 1}. {prediccion}", 'cyan'))
+    
+    while True:
+        try:
+            seleccion = int(input(colored("Ingrese el número de la predicción deseada: ", 'yellow')))
+            if 1 <= seleccion <= len(predicciones_disponibles):
+                seleccion = predicciones_disponibles[seleccion - 1]
+                mensaje = "Has seleccionado: "
+                print(colored("\n" + mensaje, 'green') + colored(seleccion + "\n", 'magenta'))
+                return seleccion
+            else:
+                print(colored("Opción no válida. Por favor, seleccione un número válido.", 'red'))
+        except ValueError:
+            print(colored("Opción no válida. Por favor, seleccione un número válido.", 'red'))
 
 def neighbours_menu(num_neighbours):
     while True:
         try:
-            num_vecinos = int(input(f"Ingrese el número de vecinos (menor que {num_neighbours}): "))
+            num_vecinos = int(input(colored(f"Ingrese el número de vecinos (menor que {num_neighbours}): ", 'yellow')))
             if 1 <= num_vecinos < num_neighbours:
                 return num_vecinos
             else:
-                print(f"El número de vecinos debe ser un valor válido y menor que {num_neighbours}.")
+                print(colored(f"El número de vecinos debe ser un valor válido y menor que {num_neighbours}.", 'red'))
         except ValueError:
-            print("Entrada no válida. Por favor, ingrese un número válido.")
+            print(colored("Entrada no válida. Por favor, ingrese un número válido.", 'red'))
--- Sistema_de_Recomendación/recommender-system.py
+++ Sistema_de_Recomendación/recommender-system.py
@@ -1,4 +1,6 @@
 import sys
+from tabulate import tabulate
+from termcolor import colored
 from utilities import format_data, get_correlations, normalize_matrix, denormalize_matrix, read_file
 from prediction import simple_prediction, mean_difference
 from menus import metrics_menu, predictions_menu, neighbours_menu
@@ -8,6 +10,15 @@ def display_help():
     print("Descripción: Este programa es un sistema recomendador que utiliza métricas para predecir la valoración de un usuario.")
     print("Argumentos:")
     print("  archivo.txt: El archivo que contiene los datos de entrada para el sistema recomendador.")  
+
+def normalize_and_round(data, decimal_places):
+  # Se normaliza y desnormaliza la matriz de valoraciones para que los valores estén entre los límites especificados
+  data = denormalize_matrix(normalize_matrix(data), float(límite_inferior), float(límite_superior))
+  for i in range(len(data)):
+      for j in range(len(data[i])):
+          data[i][j] = round(data[i][j], decimal_places)
+
+  return data
 
 if __name__ == '__main__':
   if len(sys.argv) == 2 and sys.argv[1] == "--help":
@@ -25,21 +36,20 @@ if __name__ == '__main__':
 
   prediction = predictions_menu()
 
-  num_neighbours = neighbours_menu(len(data))
+  num_neighbours = neighbours_menu(len(data)) 
 
+  # Se itera por cada usuario para predecir sus valoraciones y se almacenan el resultado en la matriz de valoraciones original
   for i in range(len(format_data(data)[1])):
     if prediction == 'Predicción simple':
       simple_prediction(data, format_data(data)[1], get_correlations(format_data(data)[0], data, metrica), num_neighbours)
     elif prediction == 'Diferencia con la media':
       mean_difference(data, format_data(data)[0], format_data(data)[1], get_correlations(format_data(data)[0], data, 'Correlacion de Pearson'), num_neighbours)
 
-  data = denormalize_matrix(normalize_matrix(data), float(límite_inferior), float(límite_superior))
+  print('\nMatriz de valoraciones')
 
-  print('\nMatriz de valoraciones: \n')
-  for row in data:
-    formatted_row = [round(value, 4) for value in row]
-    print(formatted_row)
+  data = normalize_and_round(data, 2)
 
+  print(colored(tabulate(data, tablefmt='fancy_grid'),'green'))
 
-
+  
 
