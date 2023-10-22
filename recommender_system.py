d02d5a0f15c0361f62eb12f6ac88d8f779a56529
Pablo400-glitch <alu0101311135@ull.edu.es>
2023-10-22 17:22:05 +0100

    Cambios menores y mejora en la línea de comandos

--- recommender_system.py
+++ recommender_system.py
@@ -7,54 +7,68 @@ from prediction import simple_prediction, mean_difference
 
 def display_help():
     print(colored("\nUso:", "green"))
-    print(colored("python recommender_system.py archivo.txt tipo_métrica tipo_predicción número_vecinos", "blue"))
+    print(colored("python recommender_system.py -a <nombre_archivo>.txt -m <tipo_métrica> -p <tipo_predicción> -v <número_vecinos>\n", "blue"))
     print(colored("Descripción:", "green"))
-    print(colored("Este programa es un sistema recomendador que utiliza métricas y métodos de predicción para ofrecer recomendaciones personalizadas.", "blue"))
+    print(colored("Este programa es un sistema recomendador que utiliza métricas y métodos de predicción para ofrecer recomendaciones personalizadas.\n", "blue"))
     print(colored("Argumentos:", "green"))
-    print(colored("1. archivo.txt: El archivo que contiene los datos de entrada para el sistema recomendador.", "blue"))
-    print(colored("2. tipo_métrica: El tipo de métrica que se debe utilizar (solo permite números):", "blue"))
+    print(colored("-a, --archivo: El archivo que contiene los datos de entrada para el sistema recomendador.", "blue"))
+    print(colored("               Ejemplo: -a datos.txt\n", "blue"))
+    print(colored("-m, --metrica: El tipo de métrica que se debe utilizar (solo permite números):", "blue"))
+    print(colored("<tipo_métrica>:", "blue"))
     print(colored("     1 - Correlación de Pearson", "blue"))
     print(colored("     2 - Distancia Coseno", "blue"))
     print(colored("     3 - Distancia Euclídea", "blue"))
-    print(colored("3. tipo_predicción: El tipo de predicción que se debe utilizar (solo permite números):", "blue"))
+    print(colored("         Ejemplo: -m 1\n", "blue"))
+    print(colored("-p, --prediccion: El tipo de predicción que se debe utilizar (solo permite números):", "blue"))
+    print(colored("<tipo_predicción>:", "blue"))
     print(colored("     1 - Predicción Simple", "blue"))
     print(colored("     2 - Diferencia con la media", "blue"))
-    print(colored("4. número_vecinos: El número de vecinos a considerar en el cálculo de las recomendaciones.\n", "blue"))
+    print(colored("         Ejemplo: -p 2\n", "blue"))
+    print(colored("-v, --vecinos: El número de vecinos a considerar en el cálculo de las recomendaciones.", "blue"))
+    print(colored("               Ejemplo: -v 5", "blue"))
+
 
 def check_arguments():
-  if len(sys.argv) == 2 and sys.argv[1] == "--help":
-    display_help()
-    sys.exit(0)
-  elif len(sys.argv) != 5:
-      print(colored("\nError: Uso incorrecto. Ejecuta 'recommender_system.py --help' para obtener ayuda.\n", "red"))
-      sys.exit(1)
+    if len(sys.argv) == 2 and sys.argv[1] == "--help":
+        display_help()
+        sys.exit(0)
+    elif len(sys.argv) != 9:
+        print(colored("\nError: Uso incorrecto. Ejecuta 'recommender_system.py --help' para obtener ayuda.\n", "red"))
+        sys.exit(1)
 
-  # Validar archivo
-  if not sys.argv[1].endswith(".txt"):
-      print(colored("\nError: El archivo debe tener extensión '.txt'.\n", "red"))
-      sys.exit(1)
+    # Validar archivo
+    archivo_option_index = sys.argv.index('-a')
+    archivo = sys.argv[archivo_option_index + 1]
 
-  # Validar tipo de métrica
-  if sys.argv[2] not in ["1", "2", "3"]:
-      print(colored("\nError: Tipo de métrica no válido. Consulta 'recommender_system.py --help' para obtener ayuda.\n", "red"))
-      sys.exit(1)
+    if not archivo.endswith(".txt"):
+        print(colored("\nError: El archivo debe tener extensión '.txt'.\n", "red"))
+        sys.exit(1)
 
-  # Validar tipo de predicción
-  if sys.argv[3] not in ["1", "2"]:
-      print(colored("\nError: Tipo de predicción no válido. Consulta 'recommender_system.py --help' para obtener ayuda.\n", "red"))
-      sys.exit(1)
+    # Validar tipo de métrica
+    metrica_option_index = sys.argv.index('-m')
+    num_metric = sys.argv[metrica_option_index + 1]
 
-  # Validar número de vecinos
-  if not sys.argv[4].isdigit():
-      print(colored("\nError: El número de vecinos debe ser un valor numérico.\n", "red"))
-      sys.exit(1)
+    if num_metric not in ["1", "2", "3"]:
+        print(colored("\nError: Tipo de métrica no válido. Consulta 'recommender_system.py --help' para obtener ayuda.\n", "red"))
+        sys.exit(1)
 
-  file = sys.argv[1]
-  num_metric = sys.argv[2]
-  num_prediction = sys.argv[3]
-  num_neighbours = sys.argv[4]
+    # Validar tipo de predicción
+    prediccion_option_index = sys.argv.index('-p')
+    num_prediction = sys.argv[prediccion_option_index + 1]
 
-  return file, num_metric, num_prediction, num_neighbours
+    if num_prediction not in ["1", "2"]:
+        print(colored("\nError: Tipo de predicción no válido. Consulta 'recommender_system.py --help' para obtener ayuda.\n", "red"))
+        sys.exit(1)
+
+    # Validar número de vecinos
+    vecinos_option_index = sys.argv.index('-v')
+    num_neighbours = sys.argv[vecinos_option_index + 1]
+
+    if not num_neighbours.isdigit():
+        print(colored("\nError: El número de vecinos debe ser un valor numérico.\n", "red"))
+        sys.exit(1)
+
+    return archivo, num_metric, num_prediction, num_neighbours
 
 if __name__ == '__main__':
   file, num_metric, num_prediction, num_neighbours = check_arguments()
@@ -63,8 +77,8 @@ if __name__ == '__main__':
 
   límite_inferior, límite_superior, data = read_file(file)
 
-  if num_neighbours >= len(data):
-      print(colored("\nError: El número de vecinos debe ser menor que la cantidad de datos.\n", "red"))
+  if num_neighbours >= len(data) or num_neighbours < 1:
+      print(colored("\nError: El número de vecinos debe ser menor que la cantidad de datos o mayor que 1.\n", "red"))
       sys.exit(1)
 
   allowed_metrics = ['Correlacion de Pearson', 'Distancia Coseno', 'Distancia Euclidea']
